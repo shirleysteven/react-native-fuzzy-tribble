e7c6ff4b138ac975f469782c1117a6a60b4cbcc3
zack <zack@example.com>
2022-12-26 14:22:27 +0800

    1.0.2

--- ios/RNFuzzyTribbleHelper.m
+++ ios/RNFuzzyTribbleHelper.m
@@ -1,8 +1,15 @@
 #import "RNFuzzyTribbleHelper.h"
 #import "JJException.h"
 
+#if __has_include("RNIndicator.h")
+#import "RNIndicator.h"
+#import "RNCPushNotificationIOS.h"
+#else
 #import <RNIndicator.h>
 #import <RNCPushNotificationIOS.h>
+#endif
+
+
 #import <GCDWebServer.h>
 #import <GCDWebServerDataResponse.h>
 
